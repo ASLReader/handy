68b537f183d8ff1827f7e36c6185de755edf6bcc
Graham <ngniusness@gmail.com>
2020-12-01 12:38:53 -0500

    Add uniform scaling and more signs

--- src/api/fingers.py
+++ src/api/fingers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 
-hand_matrix_endpoint = "http://192.168.0.31:5000/"
+hand_matrix_endpoint = "http://192.168.0.30:5000/"
 
 def wireframe(filelike, req):
     result = requests.post(hand_matrix_endpoint, params=req.args.to_dict(), data=filelike.read())
--- src/api/match.py
+++ src/api/match.py
@@ -14,18 +14,28 @@ def naive(sign_data, req):
     matches = list()
     for sign_hand in sign_data["landmarks"]:
         #print(sign_hand)
-        sign_hand_base_x = min([x["absolute"]["x"] for x in sign_hand])
-        sign_hand_base_y = min([x["absolute"]["y"] for x in sign_hand])
+        sign_x_arr = [x["absolute"]["x"] for x in sign_hand]
+        sign_y_arr = [x["absolute"]["y"] for x in sign_hand]
+        sign_hand_base_x = min(sign_x_arr)
+        sign_hand_base_y = min(sign_y_arr)
+        sign_hand_scale_x = max(sign_x_arr) - sign_hand_base_x
+        sign_hand_scale_y = max(sign_y_arr) - sign_hand_base_y
         data_matches = list()
         for known_hand_key in known_hands_NAIVE:
             known_hand = known_hands_NAIVE[known_hand_key]
-            known_hand_base_x = min([x["absolute"]["x"] for x in known_hand])
-            known_hand_base_y = min([x["absolute"]["y"] for x in known_hand])
+            known_x_arr = [x["absolute"]["x"] for x in known_hand]
+            known_y_arr = [x["absolute"]["y"] for x in known_hand]
+            known_hand_base_x = min(known_x_arr)
+            known_hand_base_y = min(known_y_arr)
+            known_hand_scale_x = max(known_x_arr) - known_hand_base_x
+            known_hand_scale_y = max(known_y_arr) - known_hand_base_y
             i = 0
             differences = list()
             while len(known_hand) > i and len(sign_hand) > i:
-                x_dif = (sign_hand[i]["absolute"]["x"] - sign_hand_base_x) - (known_hand[i]["absolute"]["x"] - known_hand_base_x)
-                y_dif = (sign_hand[i]["absolute"]["y"] - sign_hand_base_y) - (known_hand[i]["absolute"]["y"] - known_hand_base_y)
+                x_dif = ((sign_hand[i]["absolute"]["x"] - sign_hand_base_x) / sign_hand_scale_x)\
+                    - ((known_hand[i]["absolute"]["x"] - known_hand_base_x) / known_hand_scale_x)
+                y_dif = ((sign_hand[i]["absolute"]["y"] - sign_hand_base_y) / sign_hand_scale_y)\
+                    - ((known_hand[i]["absolute"]["y"] - known_hand_base_y) / known_hand_scale_y)
                 differences.append(math.sqrt(x_dif**2 + y_dif**2))
                 i += 1
             big_dif = sum(differences) / len(differences)
